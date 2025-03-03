0bbbd00f927ba1f4597a56864a5b9fae06d36301
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2025-03-03 13:05:28 +0100

    fixed issue

--- app/components/Home/Footer/Footer.tsx
+++ app/components/Home/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { title } from "process";
 import React from "react";
 import {
   FaClock,
@@ -10,39 +9,9 @@ import {
   FaPhoneAlt,
   FaTwitter,
 } from "react-icons/fa";
+import { footerSections } from "../../constant/constant";
 
 const Footer = () => {
-  const footerSections = [
-    {
-      title: "About Us",
-      links: [
-        { href: "/support", text: "Support Center" },
-        { href: "/customer-support", text: "Customer Support" },
-        { href: "/about", text: "About Us" },
-        { href: "/copyright", text: "Copyright" },
-        { href: "/campaigns", text: "Popular Campaign" },
-      ],
-    },
-    {
-      title: "Information",
-      links: [
-        { href: "/policy", text: "Return Policy" },
-        { href: "/privacy", text: "Privacy Policy" },
-        { href: "/terms", text: "Terms & Condition" },
-        { href: "/site", text: "Site Map" },
-        { href: "/store", text: "Store Hours" },
-      ],
-    },
-    {
-      title: "Contact Info",
-      contactInfo: {
-        phone: "+123 456 7890",
-        email: "sonibaretimi@gmail.com",
-        time: "7 Days - 8am - 10am",
-        address: " plot 4 Amsterdam,Netherland",
-      },
-    },
-  ];
   return (
     <div className="bg-white py-10" id="Footer">
       <div className="w-[90%] mx-auto px-4 sm:px-6 lg:px-8">
--- app/components/constant/constant.ts
+++ app/components/constant/constant.ts
@@ -30,3 +30,35 @@ export const navLinks = [
     label: "Contact",
   },
 ];
+
+export const footerSections = [
+  {
+    title: "About Us",
+    links: [
+      { href: "/support", text: "Support Center" },
+      { href: "/customer-support", text: "Customer Support" },
+      { href: "/about", text: "About Us" },
+      { href: "/copyright", text: "Copyright" },
+      { href: "/campaigns", text: "Popular Campaign" },
+    ],
+  },
+  {
+    title: "Information",
+    links: [
+      { href: "/policy", text: "Return Policy" },
+      { href: "/privacy", text: "Privacy Policy" },
+      { href: "/terms", text: "Terms & Condition" },
+      { href: "/site", text: "Site Map" },
+      { href: "/store", text: "Store Hours" },
+    ],
+  },
+  {
+    title: "Contact Info",
+    contactInfo: {
+      phone: "+123 456 7890",
+      email: "sonibaretimi@gmail.com",
+      time: "7 Days - 8am - 10am",
+      address: " plot 4 Amsterdam,Netherland",
+    },
+  },
+];
